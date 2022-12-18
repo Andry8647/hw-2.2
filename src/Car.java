6bc5df7e046bc766445568e57cb2b536a1b50e31
Андрей <honor10love2@gmail.com>
2022-12-18 13:59:00 +0300

    Сделал конструктор

--- src/Car.java
+++ src/Car.java
@@ -5,6 +5,14 @@ public class Car {
     String color;
     int year;
     String country;
+    Car(String brand, String madel, double engineVolume, String color, int year, String country){
+        this.brand = brand;
+        this.madel = madel;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
 
     void printCar(){
         System.out.println(brand + " " + madel + " " + year + " года выпуска, сборка "  +  country + " ," + color + " цвет " + " объем двигателя " + engineVolume);
