c36c88ea7310234b72d65931db87e1a7edfdf4a5
Андрей <honor10love2@gmail.com>
2022-12-26 19:24:31 +0300

    Выполнил доп условия

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
+import transport.Car;
+
 public class Main {
     public static void main(String[] args){
         Car lada = new Car(null,"Granta",1.7, "Желтый", 2015,"Россия");
+        lada.setRezSummer(true);
+
         Car audi = new Car("Audi",null,3.0, "Черный", 2020,"Германия");
         Car bmw = new Car("BMW","Z8",-3.0, "Черный", 2021,"Германия");
         Car kia = new Car("Kia","Sportage 4-го поколения",2.4, null, 2018,"Южная Корея");
         Car hyundai = new Car("Hyundai","Avante",1.6, "Оранжевый", -2016,null);
-        lada.printCar();
-        audi.printCar();
-        bmw.printCar();
-        kia.printCar();
-        hyundai.printCar();
+        printOnCar(lada);
+        printOnCar(audi);
+        printOnCar(bmw);
+        printOnCar(kia);
+        printOnCar(hyundai);
+
+    }
+    public static void printOnCar( Car car){
+        System.out.println(car.getBrand() + " "
+                + car.getMadel() + " " + car.getYear()
+                + " года выпуска, сборка "  +  car.getCountry()
+                + " ," + car.getColor() + " цвет " + " объем двигателя "
+                + car.getEngineVolume()+
+                car.getKorobka() + " " +car.getKuzov() + " " + car.getNumber() + " " +car.getMest() + " " + car.isRezSummer() +
+                (car.getKey().isWithoutKeyAccess() ? "Бесключевой доступ": "Ключевой доступ")+
+                (car.getKey().isRemoteRunEngine() ? "Удаленный доступ": "Ключевой доступ")+
+                "Номер страховки "+ car.getInsurance().getNumber() + "Стоимость страховки " + car.getInsurance().getCost() +
+                "Срок действия страховки " + car.getInsurance().getExpireDate());
+
+
+
 
     }
 }
---
+++ src/transport/Car.java
@@ -0,0 +1,287 @@
+package transport;
+
+import java.lang.invoke.StringConcatException;
+import java.time.LocalDate;
+
+public class Car {
+   private final String brand;
+   private final String madel;
+    private double engineVolume;
+    private String color;
+    private final int year;
+    private final String country;
+    private String korobka;
+    private final  String kuzov;
+    private String number;
+    private final int mest;
+    private boolean rezSummer;
+    private Key key;
+    private Insurance insurance;
+
+    public Car(String brand,
+               String madel,
+               double engineVolume,
+               String color,
+               int year,
+               String country,
+               String korobka,
+               String kuzov,
+               String number,
+               int mest,
+               boolean rezSummer,
+               Key key,
+               Insurance insurance) {
+        if (brand != null){
+            this.brand = brand;
+        }else {
+            this.brand = "default";
+        }
+        if(madel!= null){
+            this.madel = madel;
+        }else {
+            this.madel = "default";
+        }
+        if(engineVolume>0){
+            this.engineVolume =engineVolume;
+        }else {
+            this.engineVolume = 1.5;
+        }
+        if(color!=null){
+            this.color = color;
+        }else {
+            this.color = "Белый";
+        }
+        if (year>0){
+            this.year = year;
+        }else {
+            this.year = 2000;
+        }
+        if (country!=null){
+            this.country = country;
+        }else {
+            this.country = "default";
+        }
+        if (korobka!=null){
+            this.korobka = korobka;
+        }else {
+            this.korobka = "МКПП";
+        }
+        if (kuzov!=null){
+            this.kuzov = kuzov;
+        }else {
+            this.kuzov = "седан";
+        }
+        if (number!=null){
+            this.number = number;
+        }else {
+            this.number = "X000XX152";
+        }
+        if(key == null){
+            this.key = new Key();
+        }else {
+            this.key = key;
+        }
+        if(insurance == null){
+            this.insurance = new Insurance();
+        }else {
+            this.insurance = insurance;
+        }
+            this.mest = mest;
+        this.rezSummer = rezSummer;
+    }
+
+    public Car(String brand,
+               String madel,
+               double engineVolume,
+               String color,
+               int year,
+               String country
+               ){
+        this(
+                brand,
+                madel,
+                engineVolume,
+                color,
+                year,
+                country,
+                "МКПП",
+                "Седан",
+                "X000XX152",
+                5,true,
+                new Key(),
+                new Insurance()
+        );
+    }
+
+    public Insurance getInsurance() {
+        return insurance;
+    }
+
+    public void setInsurance(Insurance insurance) {
+        this.insurance = insurance;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getKorobka() {
+        return korobka;
+    }
+
+    public void setKorobka(String korobka) {
+        if(this.korobka == null){
+            this.korobka = "МКПП";
+        }else {
+            this.korobka = korobka;
+        }
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        if(this.number == null){
+            this.number = "X000XX152";
+        }else {
+            this.number = number;
+        }
+    }
+
+    public boolean isRezSummer() {
+        return rezSummer;
+    }
+
+    public void setRezSummer(boolean rezSummer) {
+        this.rezSummer = rezSummer;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getMadel() {
+        return madel;
+    }
+
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getKuzov() {
+        return kuzov;
+    }
+
+    public int getMest() {
+        return mest;
+    }
+    public void changeSummerRez(){
+        rezSummer = !rezSummer;
+    }
+    public boolean carNumber(){
+        if(number.length()!=9){
+            return false;
+
+        }
+        char[] chars = number.toCharArray();
+        if(!Character.isAlphabetic(chars[0])||!Character.isAlphabetic(chars[4])||!Character.isAlphabetic(chars[5])){
+            return false;
+        }
+        if(!Character.isDigit(chars[1]) ||!Character.isDigit(chars[2]) ||!Character.isDigit(chars[3]) || !Character.isDigit(chars[6]) ||!Character.isDigit(chars[7]) ||!Character.isDigit(chars[8]) ){
+            return false;
+        }
+        return true;
+    }
+    public static class Key{
+        private final boolean remoteRunEngine;
+        private final boolean withoutKeyAccess;
+        public Key(boolean remoteRunEngine, boolean withoutKeyAccess){
+            this.remoteRunEngine = remoteRunEngine;
+            this.withoutKeyAccess = withoutKeyAccess;
+        }
+        public Key(){
+            this(false,false);
+        }
+
+        public boolean isRemoteRunEngine() {
+            return remoteRunEngine;
+        }
+
+        public boolean isWithoutKeyAccess() {
+            return withoutKeyAccess;
+        }
+    }
+    public static class Insurance {
+        private final LocalDate expireDate;
+        private final double cost;
+        private final String number;
+
+        public Insurance(LocalDate expireDate, double cost, String number) {
+            if (expireDate == null) {
+                this.expireDate = LocalDate.now().plusDays(365);
+            } else {
+                this.expireDate = expireDate;
+            }
+
+            this.cost = cost;
+            if (number == null) {
+                this.number = "123456789";
+            } else {
+                this.number = number;
+            }
+        }
+        public Insurance(){
+            this(null,10000,null);
+        }
+
+        public LocalDate getExpireDate() {
+            return expireDate;
+        }
+
+        public double getCost() {
+            return cost;
+        }
+
+        public String getNumber() {
+            return number;
+        }
+        public void checkExpireDate(){
+            if(expireDate.isBefore(LocalDate.now())|| expireDate.isEqual(LocalDate.now())){
+                System.out.println("Нужно оформить новую страховку");
+            }
+        }
+        public void checkNumber(){
+            if(number.length()!=9){
+                System.out.println("Номер страховки не корректный");
+            }
+        }
+    }
+
+
+}
